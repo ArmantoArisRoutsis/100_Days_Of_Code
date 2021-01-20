1a2acb799f39989688edcd658bdb88c13f449ddc
Routsis Armanto <ar.routsis@gmail.com>
2021-01-20 03:57:15 +0200

    Added error handling in day 28 project

--- Day-28__NATO-Phonetic-Alphabet/main.py
+++ Day-28__NATO-Phonetic-Alphabet/main.py
@@ -1,10 +1,18 @@
 import pandas
+from replit import clear
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 new_data = {row.letter:row.code for (index,row) in data.iterrows()}
+go_on = ""
+while go_on != "EXIT":
+  word = input("Enter a word: ").upper()
 
-word = input("Enter a word: ").upper()
-
-for i in word:
-    print(f"{i} as {new_data[i]"}
+  try:
+    for i in word:
+        print(f"{i} as {new_data[i]}")
+  except KeyError:
+    print("Please input a word")
+  else:
+    go_on = input("\nWould you like to input another word?\n(Type 'exit' to exit the program or anything else to continue): ").upper()
+    clear()
